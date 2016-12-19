b94865ea7bb22eec7c17dffebae2ecda0dc7cf5b
William Henney <will@henney.org>
2016-12-19 22:31:01 +0000

    Add shell scripts for multibinning all mean and sigma maps

---
+++ all-sigma-maps-multibin.sh
@@ -0,0 +1,6 @@
+D=../OrionMuse/LineMaps
+linelist=$D/sigma-*[0-9][0-9][0-9][0-9]-patfixx.fits
+for line in $linelist; do
+    echo "Processing $line"
+    time python multibin-map.py $line > ${line}-multibin.log
+done
