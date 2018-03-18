bde17f53ff4e8a580fc57363f3c43eb11c686cb5
William Henney <will@henney.org>
2018-03-17 23:27:03 -0600

    Plot grain potential versus ionization

---
+++ cloudytab.py
@@ -0,0 +1,45 @@
+from astropy.table import Table
+import glob
+
+# File extensions that might be present, but which are NOT Cloudy save files
+IGNORE_EXTS = ["pdf", "png", "jpg"]
+
+class CloudyModel(object):
+    """Lightweight wrapper for output from Cloudy run 
+
+    For example:
+
+    >>> from cloudytab import CloudyModel
+    >>> m = CloudyModel("myfolder/mymodel")
+
+    `m.files` contains a list of all the files that were found: 
+              `['myfolder/mymodel.in', 'myfolder/mymodel.ovr', ETC]`
+
+    `m.data` contains dict of astropy.Table's, one for each save file:
+              `{'ovr': <Table length=289> ..., ETC}`
+
+    `m.io['in']` and `m.io['out']` contain the input and output streams
+    """
+    def __init__(self, prefix):
+        self.files = glob.glob(prefix + ".*")
+        self.data = {}
+        self.io = {}
+        for file_ in self.files:
+            saveid = file_.split(".")[-1]
+            if saveid in IGNORE_EXTS:
+                # Figure files, etc need to be skipped
+                pass
+            elif saveid in ["in", "out"]:
+                # Special case of input and output files
+                with open(file_) as f:
+                    # Just save the whole file as a string
+                    self.io[saveid] = f.read()
+            else:
+                # Assume all else are save files
+                try:
+                    self.data[saveid] = Table.read(
+                        file_, delimiter="\t",
+                        format="ascii.commented_header")
+                except UnicodeDecodeError:
+                    # Binary files can raise this error - ignore them
+                    pass
