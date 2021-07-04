8f8c11a288fad85886c3e150879bc00077776adf
William Henney <will@henney.org>
2021-07-03 19:16:30 -0500

    Save all skipped files (plus their reasons) in their own dict

--- cloudytab.py
+++ cloudytab.py
@@ -5,31 +5,37 @@ import glob
 # File extensions that might be present, but which are NOT Cloudy save files
 IGNORE_EXTS = ["pdf", "png", "jpg"]
 
-class CloudyModel(object):
-    """Lightweight wrapper for output from Cloudy run 
+
+class CloudyModel:
+    """
+    Lightweight wrapper for output from Cloudy run
 
     For example:
 
     >>> from cloudytab import CloudyModel
     >>> m = CloudyModel("myfolder/mymodel")
 
-    `m.files` contains a list of all the files that were found: 
+    `m.files` contains a list of all the files that were found:
               `['myfolder/mymodel.in', 'myfolder/mymodel.ovr', ETC]`
 
     `m.data` contains dict of astropy.Table's, one for each save file:
               `{'ovr': <Table length=289> ..., ETC}`
 
     `m.io['in']` and `m.io['out']` contain the input and output streams
+
+    `m.skipped` contains a dict of each extensions that was skipped with its reason.
     """
-    def __init__(self, prefix):
+
+    def __init__(self, prefix: str):
         self.files = glob.glob(prefix + ".*")
         self.data = {}
         self.io = {}
+        self.skipped = {}
         for file_ in self.files:
             saveid = file_.split(".")[-1]
             if saveid in IGNORE_EXTS:
                 # Figure files, etc need to be skipped
-                pass
+                self.skipped[saveid] = "Extension is listed in IGNORE_EXTS"
             elif saveid in ["in", "out"]:
                 # Special case of input and output files
                 with open(file_) as f:
@@ -39,11 +45,15 @@ class CloudyModel(object):
                 # Assume all else are save files
                 try:
                     self.data[saveid] = Table.read(
-                        file_, delimiter="\t", guess=False, fast_reader=False,
-                        format="ascii.commented_header")
+                        file_,
+                        delimiter="\t",
+                        guess=False,
+                        fast_reader=False,
+                        format="ascii.commented_header",
+                    )
                 except UnicodeDecodeError:
                     # Binary files can raise this error - ignore them
-                    pass
+                    self.skipped[saveid] = "Table.read() raised UnicodeDecodeError"
                 except InconsistentTableError:
                     # The "save heating" files can raise this error - skip them
-                    pass
+                    self.skipped[saveid] = "Table.read() raised InconsistentTableError"
