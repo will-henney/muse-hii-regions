e7af878a1f10296f411aea332ab8ef0ed1b757df
William Henney <will@henney.org>
2022-04-30 18:48:08 -0500

    Remove the tetrabloks subtree

--- lib/tetrabloks/all-musedata-maps-multibin.sh
+++
@@ -1,5 +0,0 @@
-D=~/tmp/musedata
-for line in $D/{sigma,mean,linesum}-*-[0-9][0-9][0-9][0-9].fits; do
-    echo "Processing $line"
-    time python multibin-map.py $line > ${line}-multibin.log 2>&1
-done
