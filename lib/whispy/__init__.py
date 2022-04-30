892b6eeae32bc639c544fe4bebd8d48365b5cd0b
William Henney <will@henney.org>
2022-04-30 17:26:11 -0500

    Make a new package called whispy that has my libraries
    Files are moved into a sub-folder, but I have now copied them into a
    separate project: https://github.com/will-henney/whispy
    
    I will delete them here on a follow-up commit

--- lib/whispy/__init__.py
+++ lib/whispy/__init__.py
@@ -1,7 +1,10 @@
 """
 Utilities for analyzing MUSE spectra (and from other spectrographs too)
 
-This package still has no name ...
+This package now has a name ...
+
+Whispy - Will Henney Imaging Spectroscopy in Python
+
 
 Will Henney, 2021, 2022
 
