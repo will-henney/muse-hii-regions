4e650ec8e6186144ea75f1b3fe4bc9946abfe21f
William Henney <will@henney.org>
2023-05-21 00:04:44 -0600

    Look into the Orion spectrum to see if we see 9029 DRL
    We do not

--- lib/median_continuum.py
+++ lib/median_continuum.py
@@ -32,28 +32,34 @@ def main(
     two_pass: bool = False,
     first_window_size: int = 11,
     shave_threshold: float = 1.0,
+    hdu_index: int = 1,
 ):
     """Find and remove continuum from cube by median filtering"""
 
+    # Convert data_path to Path object if it is a string
+    if isinstance(data_path, str):
+        data_path = Path(data_path)
+
     hdulist = fits.open(data_path / cube_name)
+    hdu = hdulist[hdu_index]
 
     if two_pass:
         # First filter pass
-        cont_data = get_median_continuum(hdulist[1].data, first_window_size)
+        cont_data = get_median_continuum(hdu.data, first_window_size)
         # Save off the lines that go more than shave_threshold above continuum
-        shaved_data = np.minimum(hdulist[1].data, cont_data + shave_threshold)
+        shaved_data = np.minimum(hdu.data, cont_data + shave_threshold)
         # Second filter pass
         cont_data = get_median_continuum(shaved_data, window_size)
     else:
-        cont_data = get_median_continuum(hdulist[1].data, window_size)
+        cont_data = get_median_continuum(hdu.data, window_size)
 
     # Write out the new cubes
     for data, label in [
         (cont_data, "cont"),  # Continuum
-        (hdulist[1].data - cont_data, "csub"),  # Original minus continuum
-        (hdulist[1].data / cont_data, "cdiv"),  # Original over continuum
+        (hdu.data - cont_data, "csub"),  # Original minus continuum
+        (hdu.data / cont_data, "cdiv"),  # Original over continuum
     ]:
-        fits.PrimaryHDU(header=hdulist[1].header, data=data).writeto(
+        fits.PrimaryHDU(header=hdu.header, data=data).writeto(
             f"{out_prefix}-{label}-{window_size:03d}.fits",
             overwrite=True,
         )
