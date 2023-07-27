c4daec1a6736d389b47615d256988f1686d6b6a0
William Henney <will@henney.org>
2023-07-26 21:37:38 -0600

    Finish the new big table, except for the reddening correction

--- scripts/convert-table-known-lines-latex.py
+++ scripts/convert-table-known-lines-latex.py
@@ -45,6 +45,10 @@ def format_pair(value, uncertainty, dp=2):
 
 def format_ion(ion):
     """Convert naive representation to latex version"""
+    # Short circuit for sky lines
+    if "OH" in ion:
+        return r"Sky OH"
+
     # Strip off any surrounding brackets
     prefix, suffix = "", ""
     if ion.startswith("["):
@@ -53,6 +57,8 @@ def format_ion(ion):
     if ion.endswith("]"):
         ion = ion[:-1]
         suffix = "]"
+    # And just in case of extra brackets
+    ion = ion.strip("[]")
     # Split into element, stage
     element, stage = ion.rsplit(maxsplit=1)
     if " " in element.strip():
@@ -83,6 +89,8 @@ def extract_blends(text):
         blends = re.split("plus|and maybe", text)
     elif text.startswith("Blend with"):
         blends = text.replace("Blend with", "").split(",")
+    elif text.startswith("Doublet with components"):
+        blends = [text.split(",")[-1]]
     else:
         blends = [text]
     return blends
@@ -90,13 +98,17 @@ def extract_blends(text):
 df = pd.read_csv("known-lines-final-table.csv")
 table = []
 zones = ["0", "I", "II", "III", "IV", "MYSO"]
+# Special case for [Cl II] and He I lines that are sort of blended, but both have flux measurements
+# And also He I triplet
+skip_these_blends = ("8578", "8582", "8776", "8045", "8216", "8230", "8306")
 for _, row in df.iterrows():
     ion, wavrest = row["ID"].rsplit(maxsplit=1)
-    if row["blend"]:
+    # Special case for [N I], where I unwisely put the mean doublet wavelength in the spreadsheet
+    wavrest = wavrest.replace("5199.00", "5197.98")
+    if row["blend"] and not wavrest.startswith(skip_these_blends):
         blend_label = LETTERS.pop(0)
     else:
         blend_label = ""
-
     table.append(
         {
             r"\lambda(\text{obs})": format_pair(row["wave"], np.hypot(row["e_wave"], extra_e_wave(row, zones))),
@@ -109,7 +121,7 @@ for _, row in df.iterrows():
             },
         }
     )
-    if row["blend"]:
+    if blend_label:
         datafile = list(Path.cwd().glob(f"{row['Index']:04d}*.yaml"))[0]
         data = yaml.safe_load(datafile.read_text())
         note = data["Notes"]["ID"][0]
@@ -121,6 +133,8 @@ for _, row in df.iterrows():
                 # A bare wavelength is assumed to be the same ion
                 _ion = ion
                 _wavrest = blend
+            if _wavrest.lower() == "sky":
+                _ion, _wavrest = _ion.rsplit(maxsplit=1)
             try:
                 table.append(
                     {
